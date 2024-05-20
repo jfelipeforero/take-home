af7854eef9d3d2bbf17291fe298435de0b689e7f
jfelipeforero <felipe__2001@hotmail.com>
2024-05-19 22:04:06 -0500

    delete: build folder

--- server/build/routers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appRouter = void 0;
-const trpc_1 = require("../trpc");
-const chatgpt_router_1 = require("./chatgpt.router");
-const router = trpc_1.t.router;
-exports.appRouter = router({
-    openai: chatgpt_router_1.openaiRouter
-});
